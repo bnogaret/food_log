f3d9663729adc32119c4c0fc4435711d6ca7aa1c
Baptiste NOGARET <baptiste.nogaret@gmail.com>
2016-06-06 16:53:45 +0100

    Rename draw_rectangle to draw_bounding_boxes and modify it to be able to draw multiple bounding boxes on one image

--- src/thesis_lib/draw.py
+++ src/thesis_lib/draw.py
@@ -2,16 +2,15 @@ import numpy as np
 from skimage.draw import polygon_perimeter, polygon
 
 
-def rectangle_perimeter(image, pt1, pt2, color=0, copy=False):
+def draw_bounding_boxes(image, bbox, color=0, copy=False):
     """
-    Draw a rectangle on the image, using the representation with two points.
+    Draw rectangles on the image, using the representation with two points (pt1_x, pt1_y, pt2_x, pt2_y).
     If copy is true, it return a deep copy, otherwise it modifies directly the parameter 'image'.
     
     Parameter
     --------
     image: 2D or 3D array
-    pt1: 2D coordinate of one of the corner
-    pt2: 2D coordinate of the opposite corner of pt1
+    bbox: list of rectangles to draw on the image.
     color: integer used to draw the rectangle
     copy: boolean
     
@@ -19,17 +18,19 @@ def rectangle_perimeter(image, pt1, pt2, color=0, copy=False):
     ------
     2D array if copy is true
     """
-    
-    coor = np.array((
-        pt1,
-        (pt1[0], pt2[1]),
-        pt2,
-        (pt2[0], pt1[1])
-    ))
-    rr, cc = polygon_perimeter(coor[:, 1], coor[:, 0], image.shape)
     if copy:
-        copy = image.copy()
-        copy[rr, cc] = color
-        return copy
+        img = image.copy()
     else:
-        image[rr, cc] = color
+        img = image
+    
+    for bb in bbox:
+        coor = np.array((
+            (bb[0], bb[1]),
+            (bb[0], bb[3]),
+            (bb[2], bb[3]),
+            (bb[2], bb[1])
+        ))
+        rr, cc = polygon_perimeter(coor[:, 1], coor[:, 0], img.shape)
+        img[rr, cc] = color
+    
+    return img
