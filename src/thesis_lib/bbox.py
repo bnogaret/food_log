cdf18df8b843c3aa3aaed3ce87f7e6cf272ed344
Baptiste NOGARET <baptiste.nogaret@gmail.com>
2016-06-07 16:10:33 +0100

    Add non-maximum suppression of overlapping bounding boxes

--- src/thesis_lib/bbox.py
+++ src/thesis_lib/bbox.py
@@ -2,6 +2,8 @@
 File containing functions on bounding boxes (bbox)
 """
 
+import numpy as np
+
 def get_coordinate_resized_rectangles(base_shape, resized_shape, rectangles):
     """
     Update the rectangle's coordinates according to the resize of the image (points representing the rectangle).
@@ -84,3 +86,69 @@ def get_precision_recall_bbox(ground_truth_bbox, predicted_bbox, threshold=0.5):
     precision = correct / len(predicted_bbox)
     recall = correct / len(ground_truth_bbox)
     return precision, recall
+
+
+def non_maxima_suppression(boxes, overlapThresh=0.4):
+    """
+    Delete redundant, overlapping bounding boxes, keeping the smallest box.
+    
+    Parameters
+    ----------
+    boxes: numpy array of bbox
+    overlapThresh: typical value: between 0.3 and 0.5
+    
+    Return
+    -------
+    Numpy array (int) of the remaining boxes
+    
+    References
+    ----------
+    http://www.pyimagesearch.com/2015/02/16/faster-non-maximum-suppression-python/
+    """
+    # initialize the list of picked indexes	
+    pick = []
+ 
+    # grab the coordinates of the bounding boxes
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+
+    # compute the area of the bounding boxes and sort the bounding
+    # boxes by the bottom-right y-coordinate of the bounding box
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    # idxs = np.argsort(y2)
+    idxs = np.argsort(y2)[::-1]
+
+    # keep looping while some indexes still remain in the indexes
+    # list
+    while len(idxs) > 0:
+        # grab the last index in the indexes list and add the
+        # index value to the list of picked indexes
+        # last = len(idxs) - 1
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+
+        # find the largest (x, y) coordinates for the start of
+        # the bounding box and the smallest (x, y) coordinates
+        # for the end of the bounding box
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+        # compute the width and height of the bounding box
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+
+        # compute the ratio of overlap
+        overlap = (w * h) / area[idxs[:last]]
+
+        # delete all indexes from the index list that have
+        idxs = np.delete(idxs, np.concatenate(([last],
+                np.where(overlap > overlapThresh)[0])))
+
+    # return only the bounding boxes that were picked using the
+    # integer data type
+    return boxes[pick].astype("int")
