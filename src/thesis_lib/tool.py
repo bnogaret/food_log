d95b333a875fef39b2999672df7dedb819220ba8
Added 2015-10-06 (Baptiste.Nogaret@cranfield.ac.uk) <s242635@service1.default.domain>
2016-06-20 18:27:51 +0100

    Create a new file to handle functions specific to UEFCFOOD256 dataset + move read_bb_info_txt into this new file

--- src/thesis_lib/tool.py
+++ src/thesis_lib/tool.py
@@ -1,38 +1,6 @@
-import os
-
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage.transform import resize
-
-
-def read_bb_info_txt(path, array):
-    """
-    Read the bb_info.txt to get the rectangle coordinates and its class.
-    Append this information into the list 'array' (thus, the array is obviously 
-    MODIFIED).
-    
-    The structure of the appending value is:
-    - first column (int): the image id (file name without jpg)
-    - second (int) and third columns (int): coordinate of one of the corner
-    - fourth (int) and fifth columns (int): coordinate of the opposte corner
-    - sixth column (int): label / class of the bbox (directory name)
-    
-    Parameter
-    ---------
-    path: path to the file bb_info.txt file.
-    array: list to append the different bounding boxes
-    """
-    # Get the label from the path (name of the directory containing the file)
-    label = os.path.split(os.path.dirname(path))[1]
-
-    with open(path, 'r') as f:
-        f.readline() # skip the first line that have the column names
-        for line in f:
-            # print(line)
-            split_line = line.split()
-            
-            split_line.append(label)
-            array.append([int(i) for i in split_line])
 
 
 def display_confusion_matrix(confusion_matrix, target_names=None, fname=None, normalization=True, title='Confusion matrix', cmap=plt.cm.OrRd):
