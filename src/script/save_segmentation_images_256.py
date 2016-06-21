0063fb073de6fa70987e77d9da9fbf794c714354
Added 2015-10-06 (Baptiste.Nogaret@cranfield.ac.uk) <s242635@service1.default.domain>
2016-06-21 18:57:59 +0100

    Add first version of save_segmentation_images_256.py file to make the visualization of the segmentation process easier

---
+++ src/script/save_segmentation_images_256.py
@@ -0,0 +1,175 @@
+#! /usr/bin/env python3
+import os
+import glob
+import argparse
+
+import caffe
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from skimage.transform import resize
+
+import init_path
+
+from thesis_lib.draw import draw_bounding_boxes
+from thesis_lib.bbox import *
+from thesis_lib.cnn import *
+from thesis_lib.io import load_object
+
+
+PATH_CURRENT_DIRECTORY = os.path.dirname(__file__)
+PATH_TO_UECFOOD256 = PATH_CURRENT_DIRECTORY + "/../../data/UECFOOD256/"
+PATH_TO_IMAGE_DIR = PATH_CURRENT_DIRECTORY + "/../../img/"
+
+CAFFE_ROOT = "/scratch/s242635/caffe/caffe/"
+PATH_TO_SEGMENTATION_MODEL = CAFFE_ROOT + "/models/339fd0a938ed026692267a60b44c0c58/"
+
+PATH_TO_SEG_MODEL_DEF = PATH_TO_SEGMENTATION_MODEL + 'deploy.prototxt'
+PATH_TO_SEG_MODEL_WEIGHTS = PATH_TO_SEGMENTATION_MODEL + 'GoogleNet_SOD_finetune.caffemodel'
+PATH_TO_BBOX = PATH_TO_SEGMENTATION_MODEL + 'center100.txt'
+
+IMAGE_SIZE = (224, 224)
+MEAN_BGR_VALUE = np.asarray([103.939, 116.779, 123.68])
+
+
+def argument_parser():
+    parser = argparse.ArgumentParser(description="Save image of segmentation")
+    parser.add_argument('-i',
+                        '--image-id',
+                        help='Id of the image (default: 97)',
+                        default=97,
+                        type=int)
+    parser.add_argument('-tn',
+                        '--threshold-net',
+                        help='Threshold of confidence for the CNN (default: 0.01)',
+                        default = 0.01,
+                        type=float)
+    parser.add_argument('-to',
+                        '--threshold-overlap',
+                        help='Threshold of overlap for the non-maxima suppression (default: 0.5)',
+                        default = 0.5,
+                        type=float)
+    args = parser.parse_args()
+    return args
+
+
+def find_image_path(image_id):
+    for filename in glob.iglob(PATH_TO_UECFOOD256 + '/**/' + str(image_id) + '.jpg', recursive=True):
+        return filename
+
+
+def vis_square(data, filename):
+    """
+    Take an array of shape (n, height, width) or (n, height, width, 3)
+    and visualize each (height, width) thing in a grid of size approx. sqrt(n) by sqrt(n)
+    """
+    
+    # normalize data for display
+    data = (data - data.min()) / (data.max() - data.min())
+    
+    # force the number of filters to be square
+    n = int(np.ceil(np.sqrt(data.shape[0])))
+    padding = (((0, n ** 2 - data.shape[0]),
+               (0, 1), (0, 1))                 # add some space between filters
+               + ((0, 0),) * (data.ndim - 3))  # don't pad the last dimension (if there is one)
+    data = np.pad(data, padding, mode='constant', constant_values=1)  # pad with ones (white)
+    
+    # tile the filters into an image
+    data = data.reshape((n, n) + data.shape[1:]).transpose((0, 2, 1, 3) + tuple(range(4, data.ndim + 1)))
+    data = data.reshape((n * data.shape[1], n * data.shape[3]) + data.shape[4:])
+    
+    plt.imsave(filename, data)
+
+
+def get_ground_truth_bbox(image_id, initial_image_size):
+    # Load the pickle save dataframe containing 6 columns:
+    # 'image_id' (int) : image name (exclusing the '.jpg')
+    # 'x1' (int), 'y1' (int) : coordinate of one of the rectangle point
+    # 'x2' (int), 'y2' (int) : coordinate of the opposite rectangle corner
+    # 'label' (int) : class of the bbox
+    ground_truth_bbox = load_object("256_gt_bbox")
+    
+    # Get the rows of the current file
+    ground_truth = ground_truth_bbox.loc[ground_truth_bbox.image_id == image_id]
+    # Extract the numpy array corresponding to the bbox coordinates
+    ground_truth_box = ground_truth.as_matrix(["x1", "y1", "x2", "y2"])
+    
+    resized_ground_truth = get_coordinate_resized_rectangles(initial_image_size,
+                                                             IMAGE_SIZE,
+                                                             ground_truth_box)
+    
+    print(ground_truth)
+    print(resized_ground_truth)
+    
+    return resized_ground_truth
+    
+
+def save_segmentation_image(image_id, threshold_net, threshold_overlap):
+    path_img = find_image_path(image_id)
+    if path_img is None:
+        raise Exception("Unknown image id provided")
+    
+    # In my case, I'm using the cpu. To change if a GPU is enable.
+    set_mode()
+    
+    cnn_bbox_coordinates = np.loadtxt(PATH_TO_BBOX, np.float, delimiter=',')
+    
+    net = get_trained_network(PATH_TO_SEG_MODEL_DEF, PATH_TO_SEG_MODEL_WEIGHTS, IMAGE_SIZE)
+    
+    transformer = get_transformer_rgb_image(net.blobs['data'].data.shape, MEAN_BGR_VALUE)
+    
+    image = caffe.io.load_image(path_img)
+    transformed_image = transformer.preprocess('data', image)
+    print(image.shape)
+    print(transformed_image.shape)
+    
+    # Feed the data to the network
+    net.blobs['data'].data[...] = transformed_image
+
+    output = net.forward()
+    prob = output['prob'][0]
+    print(prob)
+    print(prob.shape)
+    
+    # Select indices superior to a threshold
+    predicted_index = np.where(prob > threshold_net)[0]
+    if predicted_index.size == 0:
+        predicted_index = np.where(prob == prob.max())[0]
+
+    print(cnn_bbox_coordinates)
+    print(cnn_bbox_coordinates.shape, cnn_bbox_coordinates.dtype)
+    print(cnn_bbox_coordinates[predicted_index])
+
+    # np.ceil: return a float dtype
+    predicted_boxes = np.ceil(cnn_bbox_coordinates[predicted_index] * 224)
+    resized_picture = resize(image, IMAGE_SIZE)
+
+    boxes = non_maxima_suppression(predicted_boxes, prob[predicted_index], threshold_overlap)
+    
+    resized_ground_truth = get_ground_truth_bbox(image_id, image.shape[0:2])
+    
+    fig = plt.figure(figsize=(6, 14))
+    
+    ax = plt.subplot(311)
+    ax.imshow(draw_bounding_boxes(resized_picture, resized_ground_truth, color=0, copy=True))
+    ax.axis('off')
+    ax.set_title("Ground truth boxes")
+    
+    ax = plt.subplot(312)
+    ax.imshow(draw_bounding_boxes(resized_picture, predicted_boxes, color=0, copy=True))
+    ax.axis('off')
+    ax.set_title("Predicted boxes before selection")
+    
+    ax = plt.subplot(313)
+    ax.imshow(draw_bounding_boxes(resized_picture, boxes, color=0, copy=True)) 
+    ax.axis('off')  
+    ax.set_title("Predicted boxes after selection")
+    
+    plt.savefig(PATH_TO_IMAGE_DIR + "seg_" + str(image_id) + ".jpg")
+
+
+if __name__ == "__main__":
+    args = argument_parser()
+    print(args)
+    save_segmentation_image(args.image_id, args.threshold_net, args.threshold_overlap)
+
