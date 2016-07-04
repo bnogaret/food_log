e745235240e7f75ab2e8c8e998f2fb2bc8c87c56
Added 2015-10-06 (Baptiste.Nogaret@cranfield.ac.uk) <s242635@service2.default.domain>
2016-07-04 16:33:30 +0100

    Add test to load_img_as_float

--- src/tests/test_cnn.py
+++ src/tests/test_cnn.py
@@ -12,6 +12,8 @@ import caffe
 import constants as const
 from thesis_lib.io import load_img_as_float
 from thesis_lib.cnn import get_transformer_rgb_image, transform_rgb_image
+from skimage.io import imread
+from skimage import img_as_float
 
 
 class TransformRgbTest(unittest.TestCase):
@@ -27,3 +29,20 @@ class TransformRgbTest(unittest.TestCase):
         img = transform_rgb_image(image, (224, 224), mean_bgr)
         
         assert_allclose(img, transformed_image, rtol=1e-04, atol=1e-05)
+    
+    def test_same_behaviour_between_load_img_as_float(self):
+        image_path = "color_image.jpg"
+        image = load_img_as_float(image_path)
+        img = caffe.io.load_image(image_path)
+        
+        assert_allclose(img, image, rtol=1e-04, atol=1e-05)
+        
+    def test_same_behaviour_between_load_image(self):
+        image_path = "color_image.jpg"
+        img = caffe.io.load_image(image_path)
+        
+        image = imread(image_path)
+        image = img_as_float(image).astype(np.float32)
+        
+        assert_allclose(img, image, rtol=1e-04, atol=1e-05)
+    
