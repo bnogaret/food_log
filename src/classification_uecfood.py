6b541ca0eab75846caaf9eed0bc76dc7b5522d14
Added 2015-10-06 (Baptiste.Nogaret@cranfield.ac.uk) <s242635@service2.default.domain>
2016-07-07 17:47:33 +0100

    Add hyperparameter optimization and simple classifier

--- src/classification_uecfood.py
+++ src/classification_uecfood.py
@@ -14,12 +14,13 @@ import caffe
 import pandas as pd
 
 from sklearn.linear_model import SGDClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.grid_search import GridSearchCV, RandomizedSearchCV
 from sklearn.svm import LinearSVC
-from sklearn.cross_validation import cross_val_predict
+from sklearn.cross_validation import cross_val_predict, train_test_split, ShuffleSplit
 from sklearn.metrics import classification_report, confusion_matrix
 
 from skimage.transform import resize
@@ -60,8 +61,9 @@ def argument_parser():
                              '"nb": naive bayes ' + \
                              '"svm": linear svm ' + \
                              '"ada": Adaboost classifier ' + \
+                             '"gb": Gradient Boosting ' + \
                              '"all": all the possible classifiers',
-                        choices=['sgd', 'rf', 'tree', 'knn', 'nb', 'svm', 'ada','all'],
+                        choices=['sgd', 'rf', 'tree', 'knn', 'nb', 'svm', 'ada', 'gb', 'all'],
                         default='sgd',
                         type=str)
     
@@ -71,18 +73,18 @@ def argument_parser():
                              '"100": UEC-FOOD100 ' + \
                              '"256": UEC-FOOD256',
                         choices=['256', '100'],
-                        default='100',
+                        default='256',
                         type=str)
     
     args = parser.parse_args()
     return args
 
-def classify(root_directory, descriptor, classifiers):
+def classify(root_directory, descriptor, classifiers, param_grid):
     data = []
     target = []    
     
-    for entry in list(os.scandir(root_directory))[0:4]:
-    # for entry in os.scandir(root_directory):
+    # for entry in list(os.scandir(root_directory))[0:4]:
+    for entry in os.scandir(root_directory):
         if entry.is_dir(follow_symlinks=False):
             bb_info = []
             read_bb_info_txt(entry.path + "/bb_info.txt", bb_info)
@@ -92,8 +94,8 @@ def classify(root_directory, descriptor, classifiers):
             
             print(label)
             
-            for image_path in list(glob.iglob(entry.path + '/*.jpg', recursive=False))[0:20]:
-            # for image_path in glob.iglob(entry.path + '/*.jpg', recursive=False):
+            # for image_path in list(glob.iglob(entry.path + '/*.jpg', recursive=False))[0:40]:
+            for image_path in glob.iglob(entry.path + '/*.jpg', recursive=False):
                 # print(image_path)
                 filename_without_jpg = int(os.path.basename(image_path).replace(".jpg", ''))
                 gt_bboxes = df.loc[df._img_name == filename_without_jpg].as_matrix(["_x1", "_y1", "_x2", "_y2"])
@@ -111,19 +113,44 @@ def classify(root_directory, descriptor, classifiers):
     
     X, y = descriptor.post_process_data(data, target)
     
-    CATEGORY_FILE = root_directory + "/category.txt"
-    
     for classifier in classifiers:
         print(classifier)
+        print(param_grid)
+        
+        if param_grid is not None:
+            if hasattr(classifier, 'n_estimators'):
+                classifier.n_estimators = 25 # For faster search!
+            
+            X_validation, X, y_validation, y = train_test_split(X, y, train_size=0.10)
+            
+            clf = GridSearchCV(classifier,
+                               param_grid,
+                               cv=5,
+                               scoring='accuracy',
+                               n_jobs=-1,
+                               refit=True)
+            
+            clf.fit(X_validation, y_validation)
+            
+            print("Best parameters set found on development set:")
+            print(clf.best_params_)
+            
+            classifier = clf.best_estimator_
+            
+            if hasattr(classifier, 'n_estimators'):
+                classifier.n_estimators = 500
+            
+            print(classifier)
         
         y_pred = cross_val_predict(classifier, X, y, n_jobs=4, cv=10)
         
         print(classification_report(y, y_pred))
         cm = confusion_matrix(y, y_pred)
         print(cm)
+        
         """
         save_object(cm,
-                    "cm_" + classifier.__class__.__name__,
+                    "cm_" + descriptor.__class__.__name__ + "_" + classifier.__class__.__name__,
                     overwrite=True)
         """
         """
@@ -163,23 +190,61 @@ def main():
     }
     
     arg_classifier = {
-        'sgd'   : SGDClassifier(n_iter=250, penalty='none', n_jobs=4),
-        'rf'    : RandomForestClassifier(n_estimators=250, min_samples_leaf=20, n_jobs=4),
+        'sgd'   : SGDClassifier(n_iter=500, penalty='none', n_jobs=2),
+        'rf'    : RandomForestClassifier(n_estimators=500, n_jobs=2),
         'tree'  : DecisionTreeClassifier(min_samples_leaf=10),
-        'knn'   : KNeighborsClassifier(n_neighbors=10, leaf_size=30),
+        'knn'   : KNeighborsClassifier(n_neighbors=10, leaf_size=30, n_jobs=2),
         'nb'    : GaussianNB(),
         'svm'   : LinearSVC(fit_intercept=False, dual=False),
-        'ada'   : AdaBoostClassifier()
+        'ada'   : AdaBoostClassifier(DecisionTreeClassifier(min_samples_leaf=10), n_estimators=200),
+        'gb'    : GradientBoostingClassifier(n_estimators=10)
     }
     
-    if args.classifier == "all":
+    arg_tuned = {
+        'sgd'   : [{'penalty' : ['l2', 'none'],
+                   'alpha' : [1e-2, 1e-3, 1e-4, 1e-5],
+                   'loss' : ['hinge', 'log']},
+                   {'penalty' : ['elasticnet'],
+                   'alpha' : [1e-2, 1e-3, 1e-4, 1e-5],
+                   'loss' : ['hinge', 'log'],
+                   'l1_ratio' : [0.15, 0.25]}],
+        'rf'    : {'max_depth': [5, None],
+                   'max_features': ['sqrt', 'log2'],
+                   'min_samples_split': [1, 10, 20],
+                   'min_samples_leaf': [10, 20, 30],
+                   'criterion': ['gini', 'entropy']},
+        'tree'  : {'max_features': ['sqrt', 'log2'],
+                   'min_samples_split': [10, 20],
+                   'min_samples_leaf': [10, 20],
+                   'criterion': ['gini', 'entropy']},
+        'knn'   : {'n_neighbors': [15, 10, 5],
+                   'weights': ['uniform', 'distance'],
+                   'metric': ['minkowski','euclidean','manhattan'],
+                   'leaf_size': [50, 30, 10]},
+        'nb'    : None,
+        'svm'   : {'penalty' : ['l2'],
+                   'C' : [10, 1, 0.1],
+                   'loss' : ['hinge', 'squared_hinge'],
+                   'multi_class' : ['ovr', 'crammer_singer'],
+                   'dual' : [True]},
+        'ada'   : None,
+        'gb'    : {'loss': ['deviance', 'exponential'],
+                   'max_features': ['sqrt', 'log2'],
+                   'min_samples_split': [10, 20],
+                   'min_samples_leaf': [10, 20]}
+    }
+    
+    if args.classifier == 'all':
         classifiers = arg_classifier.values()
+        param_grid = None
     else:
         classifiers = [arg_classifier[args.classifier]]
+        param_grid = arg_tuned[args.classifier]
     
     classify(arg_dataset[args.dataset],
              arg_feature[args.feature],
-             classifiers)
+             classifiers,
+             param_grid)
     
 
 if __name__ == "__main__":
