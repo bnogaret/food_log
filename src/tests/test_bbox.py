Correct bug calculation of the intersection

src/tests/test_bbox.py
@@ -51,7 +51,7 @@ class OverlapTest(unittest.TestCase):
         
         self.assertEqual(res, .25)
     
-    def test_non_overlap(self):
+    def test_simple_non_overlap(self):
         bbox = np.asarray([[0, 0, 50, 50], [100, 100, 200, 200]])
         res = get_overlap_ratio_bbox(bbox[0], bbox[1])
         
@@ -66,6 +66,20 @@ class OverlapTest(unittest.TestCase):
         expect_correct = np.asarray([[0, 0, 40, 40], [100, 100, 900, 900]])
         expect_gt = np.asarray([[0, 0, 50, 50], [0, 0, 1000, 1000]])
         expec_metrics = np.asarray((2/5, 2/3, 2/4))
+        
+        assert_array_equal(res_correct, expect_correct)
+        assert_array_equal(res_gt, expect_gt)
+        assert_array_almost_equal(res_metrics, expec_metrics)
+    
+    def test_get_correct_bbox_simple_overlap(self):
+        gt = np.asarray([[0, 0, 50, 50], [0, 0, 1000, 1000]])
+        predicted = np.asarray([[0, 0, 40, 40], [0, 0, 900, 900]])
+        
+        res_correct, res_gt, res_metrics = get_correct_bbox(gt, predicted, threshold=0.8)
+        
+        expect_correct = np.asarray([[0, 0, 900, 900]])
+        expect_gt = np.asarray([[0, 0, 1000, 1000]])
+        expec_metrics = np.asarray((1/3, 1/2, 1/2))
         
         assert_array_equal(res_correct, expect_correct)
         assert_array_equal(res_gt, expect_gt)

src/thesis_lib/bbox.py
@@ -17,9 +17,9 @@ def get_coordinate_resized_rectangles(base_shape, resized_shape, rectangles):
     Parameters
     ---------
     base_shape: array-like of 2
-        Size of the picture
+        Size of the picture (row (y), column(x))
     resized_shape: array-like of 2
-        New size of the picture
+        New size of the picture (row (y), column(x))
     rectangles: array-like of 4
         List of rectangle coordinates to resized.
 
@@ -28,8 +28,8 @@ def get_coordinate_resized_rectangles(base_shape, resized_shape, rectangles):
     :class:`np.ndarray`
         Resized coordinates
     """
-    x_scale = resized_shape[0]/base_shape[1]
-    y_scale = resized_shape[1]/base_shape[0]
+    x_scale = resized_shape[1]/base_shape[1]
+    y_scale = resized_shape[0]/base_shape[0]
     
     resized_rectangles = np.array(rectangles).astype(np.float)
     
@@ -163,10 +163,10 @@ def get_correct_bbox(ground_truth_bbox, predicted_bbox, threshold=0.5):
         # compute the intersection and union
         # max(0, min(XA2, XB2) - max(XA1, XB1)) * max(0, min(YA2, YB2) - max(YA1, YB1)))
         intersection = np.maximum(0, np.minimum(x2, p[2]) - np.maximum(x1, p[0])) * \
-                       np.maximum(0, np.minimum(y2, p[3]), - np.maximum(y1, p[1]))
+                       np.maximum(0, np.minimum(y2, p[3]) - np.maximum(y1, p[1]))
         
         area = (x2 - x1 + 1) * (y2 - y1 + 1)
-        area_p = (p[2] - p [0] + 1) * (p[3] - p[1] + 1)
+        area_p = (p[2] - p[0] + 1) * (p[3] - p[1] + 1)
         
         union = area + area_p - intersection
         
@@ -181,10 +181,7 @@ def get_correct_bbox(ground_truth_bbox, predicted_bbox, threshold=0.5):
             correct_predictions.append(p)
             found_gt_bboxes.append(gt[idx_max].copy())
             # gt = np.delete(gt, idx_max, axis=0) # Delete the index from the available ground truth bboxes
-            gt[idx_max] = 0 
-        
-        if gt.size == 0: # No more ground truth available, we can leave the loop
-            break
+            gt[idx_max] = 0
     
     correct = len(correct_predictions)
     accuracy = correct / (correct + len(predicted_bbox) - correct + len(ground_truth_bbox) - correct)