a8a0bc8f7fbf9132da3c1f6a025939f24d7298cb
Added 2015-10-06 (Baptiste.Nogaret@cranfield.ac.uk) <s242635@service2.default.domain>
2016-07-12 23:53:44 +0100

    Add execution for different threshold values of overlapping for correctness

--- src/segmentation_uecfood.py
+++ src/segmentation_uecfood.py
@@ -14,7 +14,7 @@ from skimage.io import imread
 
 from thesis_lib.segmenter import CnnSegmenter
 from thesis_lib.io import load_object
-from thesis_lib.bbox import get_accuracy_bbox, get_coordinate_resized_rectangles
+from thesis_lib.bbox import get_correct_bbox
 from thesis_lib.cnn import set_caffe_mode
 import constants as const
 
@@ -29,46 +29,44 @@ def get_gt_bbox_for_image(df, path_image):
     # print(bbox)
     
     if bbox._multi_item.all(axis=0):
-        # Case when the same image has multi-food items - multi food labels (so different absolute path)
+        # Case when the same image has multi-food items that can be from different labels (so different absolute path)
         image_name = int(os.path.basename(path_image).replace(".jpg", ''))
         bbox = df.loc[(df._img_name == image_name) & (df._multi_item == True)]
     
-    return bbox.as_matrix(["_x1", "_y1", "_x2", "_y2"])
+    return bbox.as_matrix(["_x1", "_y1", "_x2", "_y2", "_cat"])
 
 
-def segmentate(root_directory, segmenter, pickle_filename):
+def segmentate(segmenter, overlap_for_correctness, root_directory, pickle_filename):
     df = load_object(pickle_filename)
-    metrics = np.asarray([0.0, 0.0, 0.0], dtype=np.float32)
-    count = 0
     
-    # for entry in list(os.scandir(root_directory))[0:5]:
-    for entry in os.scandir(root_directory):
-        if entry.is_dir(follow_symlinks=False):
-            label = int(entry.name)
-            
-            print(label)
-            
-            # for path_image in list(glob.iglob(entry.path + '/*.jpg', recursive=False))[0:50]:
-            for path_image in glob.iglob(entry.path + '/*.jpg', recursive=False):
-                # filename_without_jpg = int(os.path.basename(path_image).replace(".jpg", ''))
-                # print(path_image)
-                gt_bboxes = get_gt_bbox_for_image(df, path_image)
+    for ofc in overlap_for_correctness:
+        metrics = np.asarray([0.0, 0.0, 0.0], dtype=np.float32)
+        count = 0
+        
+        # for entry in list(os.scandir(root_directory))[0:2]:
+        for entry in os.scandir(root_directory):
+            if entry.is_dir(follow_symlinks=False):
+                label = int(entry.name)
                 
-                image = imread(path_image)
+                # print(label)
                 
-                resized_ground_truth = get_coordinate_resized_rectangles(image.shape[0:2], const.IMAGE_SIZE, gt_bboxes)
-                pred_bboxes = segmenter.get_bbox(image)
-                
-                # print(resized_ground_truth)
-                # print(pred_bboxes)
-                
-                pred_bboxes, res = get_accuracy_bbox(resized_ground_truth, pred_bboxes)
-                
-                metrics += res
-                count += 1
-                
-    print(metrics / count)
-    
+                # for path_image in list(glob.iglob(entry.path + '/*.jpg', recursive=False))[:20]:
+                for path_image in glob.iglob(entry.path + '/*.jpg', recursive=False):
+                    # Retrieve ground truth bbox for the image
+                    gt_bboxes = get_gt_bbox_for_image(df, path_image)
+                    
+                    image = imread(path_image)
+                    
+                    pred_bboxes = segmenter.get_bbox(image)
+                    
+                    pred_bboxes, gt_bboxes, metric_res = get_correct_bbox(gt_bboxes, pred_bboxes, threshold=ofc)
+                    
+                    metrics += metric_res
+                    count += 1
+        
+        print("Overlap for correctness = ", ofc)
+        print("Metrics: ", metrics / count)
+
 
 def main():
     segmenter = CnnSegmenter(const.PATH_TO_SEG_MODEL_DEF,
@@ -81,8 +79,12 @@ def main():
     
     set_caffe_mode()
     
-    segmentate(const.PATH_TO_ROOT_UECFOOD256, segmenter, const.PICKLE_FILENAME_256_GT_BBOX)
+    # overlap_for_correctness = [1.0]
+    overlap_for_correctness = np.arange(0.5, 1.01, 0.05)
+    
+    segmentate(segmenter, overlap_for_correctness, const.PATH_TO_ROOT_UECFOOD256, const.PICKLE_FILENAME_256_GT_BBOX)
 
 
 if __name__ == "__main__":
     main()
+
